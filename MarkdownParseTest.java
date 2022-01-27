c51b5dad3a956b5e47e21d9f3f54f5679dbe869a
Emrys025 <c3gu@ucsd.edu>
2022-01-26 19:19:37 -0800

    1.26 change3

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
     public void addition() {
@@ -12,8 +14,23 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void newTest() throws IOException{
+    public void testFileTest() throws IOException{
         assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+    }
+
+    @Test
+    public void imageTest() throws IOException{
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("imageTest.md"))));
+    }
+
+    @Test
+    public void handlingEntersTest() throws IOException{
+        assertEquals(List.of("cat"), MarkdownParse.getLinks(Files.readString(Path.of("handlingEnters.md"))));
+    }
+
+    @Test
+    public void noLinkFileTest() throws IOException{
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("noLinkFile.md"))));
     }
 }
 
